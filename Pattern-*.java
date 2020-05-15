41a5657c42839eb2509aad520c9355ce755b2ca9
Ankita <65420449+ankita2002@users.noreply.github.com>
2020-05-16 01:33:23 +0530

    Create Pattern-*.java

---
+++ Pattern-*.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Program
+{
+    public static void main(String[] args) {
+        int i,j;
+        for(i=1;i<=4;i++)
+        {
+            for(j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
