a6a33bd2f6dc34437575cb40c4aeec12cf1aa8af
Ankita <65420449+ankita2002@users.noreply.github.com>
2020-05-16 01:34:16 +0530

    Create Add2no.java

---
+++ Add2no.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Add
+{
+    public static void main(String[] args) {
+        int sum;
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter 2 numbers");
+        int a= sc.nextInt();
+        int b= sc.nextInt();
+        
+        sum= a+b;
+        
+        System.out.println("Sum:"+sum);
+    }
+}
