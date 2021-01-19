e10ed2a56a6b738165959c2155560a3f05cc7921
Ankita <ankitaupadhyaya209@gmail.com>
2021-01-20 02:48:02 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,25 @@
+package Arithmetic;
+public class Arithmetic
+{
+    public int add(int x, int y)
+    {
+        return x+y;
+    }
+    public int sub(int x, int y)
+    {
+        return x-y;
+    }
+    public int mul(int x, int y)
+    {
+        return x*y;
+    }
+    public double div(int x, int y)
+    {
+        return (double)x/y;
+    }
+    public int mod(int x, int y)
+    {
+        return x%y;
+    }
+
+}
